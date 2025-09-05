3bd2046a2a062c7f7911dc6c32a1ec29344e71ce
Lei Nelissen <lei@codified.nl>
2025-09-05 15:09:29 +0200

    feat: base nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+    title: 'Hoe het Net Werkt',
+    description: 'Een visualisatie over het internet',
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={`${inter} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
