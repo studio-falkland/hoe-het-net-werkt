287e377d37821aeffa78b952367457e997c5f900
Lei Nelissen <lei@codified.nl>
2025-11-04 15:39:54 +0100

    feat: menu background

--- src/app/components/Menu/index.tsx
+++ src/app/components/Menu/index.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import styles from './index.module.css';
 
 export default function Menu() {
     return (
         <div className="fixed top-0 left-0 right-0 z-50">
-            <div className="max-w-7xl mx-auto px-4 py-4 flex items-center">
+            <div className="max-w-7xl mx-auto px-4 py-4 flex items-center z-10 relative">
                 <div>
                     <img src="/hoe-het-net-werkt-logo.svg" alt="Hoe het Net Werkt" className="h-24" />
                 </div>
@@ -11,6 +12,11 @@ export default function Menu() {
                     <Link href="#story">Verhaal</Link>
                     <Link href="#visualisations">Visualisaties</Link>
                 </div>
+            </div>
+            <div className={styles['progressive-blur']}>
+                <div className={styles.blur} />
+                <div className={styles.blur} />
+                <div className={styles.blur} />
             </div>
         </div>
     );
