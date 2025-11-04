be3f810a35e76b1b5e27bc3be9afd169d279f895
Lei Nelissen <lei@codified.nl>
2025-11-04 17:15:05 +0100

    docs: useActiveSection

--- src/app/components/Menu/useActiveSection.ts
+++ src/app/components/Menu/useActiveSection.ts
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
 
+/**
+ * Returns the active section based on the scroll position.
+ */
 export function useActiveSection() {
     const [activeSection, setActiveSection] = useState<string>('');
 
     useEffect(() => {
+        // Create an observer to track the scroll position
         const observer = new IntersectionObserver(
             (entries) => {
                 entries.forEach((entry) => {
@@ -15,12 +19,15 @@ export function useActiveSection() {
             { threshold: 0.3 },
         );
 
+        // Get the story and visualisations sections
         const storySection = document.getElementById('story');
         const visualisationsSection = document.getElementById('visualisations');
 
+        // Observe the story and visualisations sections
         if (storySection) observer.observe(storySection);
         if (visualisationsSection) observer.observe(visualisationsSection);
 
+        // Unobserve the story and visualisations sections when the component unmounts
         return () => {
             if (storySection) observer.unobserve(storySection);
             if (visualisationsSection) observer.unobserve(visualisationsSection);
