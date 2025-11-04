2a49d5ab2e178e7331a9b67e7bf719cbfc297b4d
Lei Nelissen <lei@codified.nl>
2025-11-04 11:56:44 +0100

    fix: properly initialise previewbanner state

--- src/app/components/PreviewBanner/index.tsx
+++ src/app/components/PreviewBanner/index.tsx
@@ -2,14 +2,20 @@
 import { useEffect, useState } from 'react';
 
 const LOCAL_STORAGE_KEY = 'hoe-het-net-werkt-preview-banner-dismissed';
-const LOCAL_STORAGE_VALUE = typeof window !== 'undefined' ? window?.localStorage.getItem(LOCAL_STORAGE_KEY) ?? 'true' : 'true';
 
 export default function PreviewBanner() {
-    const [isDismissed, setIsDismissed] = useState<boolean>(LOCAL_STORAGE_VALUE === 'true');
+    const [isDismissed, setIsDismissed] = useState<boolean>(true);
 
     useEffect(() => {
         window.localStorage.setItem(LOCAL_STORAGE_KEY, isDismissed.toString());
     }, [isDismissed]);
+
+    useEffect(() => {
+        const value = window?.localStorage.getItem(LOCAL_STORAGE_KEY) ?? 'true';
+        if (value === 'true') {
+            setIsDismissed(true);
+        }
+    }, []);
 
     if (isDismissed) {
         return null;
